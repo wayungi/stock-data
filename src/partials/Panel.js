b2e229cdf9d927bbb32f8dbafe3a6ba2f782bcf9
wayungi <fwayungi@gmail.com>
2022-09-01 13:16:38 +0300

    Add link to images on home page

--- src/partials/Panel.js
+++ src/partials/Panel.js
@@ -1,34 +1,47 @@
 /** @format */
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Panel = (props) => {
   const { datum } = props;
   const {
     logos: { dark },
     name,
+    id,
   } = datum;
+
   return (
-    <div>
+    <Link to={`/details/${id}`}>
       <div>
-        <img src={dark} alt={name} />
-        <p>{name}</p>
+        <div>
+          <img src={dark} alt={name} />
+        </div>
+        <div>
+          <p>{name}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
 Panel.defaultProps = {
+  id: 0,
   dark: '',
   name: 'no team',
   logos: {
     dark: '',
   },
   datum: {
-    id: 0, name: '', slug: '', abbr: '', logos: {},
+    id: 0,
+    name: '',
+    slug: '',
+    abbr: '',
+    logos: {},
   },
 };
 
 Panel.propTypes = {
+  id: PropTypes.number,
   dark: PropTypes.string,
   name: PropTypes.string,
   logos: PropTypes.instanceOf(Object),
