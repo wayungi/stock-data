9891c0bffb06ee92f7981a841e483607e6f4213e
wayungi <fwayungi@gmail.com>
2022-09-01 11:53:18 +0300

    Add provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
+/** @format */
+
+import React, { Provider } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
