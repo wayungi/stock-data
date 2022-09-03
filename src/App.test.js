6459f39ff2741eb7ff362de62eefcda060740624
wayungi <fwayungi@gmail.com>
2022-09-03 16:05:39 +0300

    Add more tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,19 +2,16 @@
 
 import React from 'react';
 import { render } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import HomePage from '../HomePage';
-import { store } from '../../redux/store';
+import App from './App';
+import { store } from './redux/store';
 
-describe('Test the HomePage component', () => {
-  test('HomePage renders correctly', () => {
+describe('Test the App component', () => {
+  test('App component renders correctly', () => {
     const tree = render(
       <React.StrictMode>
         <Provider store={store}>
-          <Router>
-            <HomePage />
-          </Router>
+          <App />
         </Provider>
       </React.StrictMode>,
     );
--- src/components/__tests__/DetailPage.test.js
+++ src/components/__tests__/DetailPage.test.js
@@ -1,19 +1,17 @@
-/** @format */
-
 import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import HomePage from '../HomePage';
+import DetailPage from '../HomePage';
 import { store } from '../../redux/store';
 
-describe('Test the HomePage component', () => {
+describe('Test the DetailPage component', () => {
   test('HomePage renders correctly', () => {
     const tree = render(
       <React.StrictMode>
         <Provider store={store}>
           <Router>
-            <HomePage />
+            <DetailPage />
           </Router>
         </Provider>
       </React.StrictMode>,
